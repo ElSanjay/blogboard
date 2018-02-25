5eeb1a41627f2b4cc343d3ccf23acb5e154816dc
Yazed Jamal <muyaszed@gmail.com>
2018-02-25 22:42:20 +0000

    new seeting for action cable in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,8 +12,8 @@ Rails.application.configure do
     config.action_mailer.default_url_options = { :host => 'blogleader.herokuapp.com' }
 
 
-  # config.middleware.use UserInfoActionCable
-  config.web_socket_server_url = "wss://blogleader.herokuapp.com/cable"
+  config.middleware.use UserInfoActionCable
+  config.web_socket_server_url = "wss://blogleader.herokuapp.com/"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -57,7 +57,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://blogleader.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://blogleader.herokuapp.com/' ]
+  # config.action_cable.allowed_request_origins = [ 'https://blogleader.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
